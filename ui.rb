c94eedc152dde72e242405d1f847d6ade68aaece
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-05-10 18:25:12 +0300

    Add files via upload

---
+++ ui.rb
@@ -0,0 +1,9 @@
+puts "Enter your name:"
+name = gets.chomp()
+puts "Enter your surname:"
+sname = gets.chomp()
+puts "Enter your age"
+age = gets.chomp()
+puts ("Your name: " + name)
+puts ("Your surname: " + sname)
+puts ("age: " + age)
